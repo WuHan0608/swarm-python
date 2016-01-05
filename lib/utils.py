efd58ef64d4d925b4193b7539851536e0927a37c
Wuhan <wuhan0608@gmail.com>
2016-01-06 02:04:43 +0800

    add lib/utils.py

---
+++ lib/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+
+def byteformat(size, unit='B'):
+    units = ('B', 'KiB', 'MiB', 'GiB', 'TiB')
+    if size < 1024:
+        return '{s:.2f} {u}'.format(s=size,u=unit)
+    return byteformat(size/1024.0, units[units.index(unit)+1])
+
+def timeformat(time, unit='second'):
+    units = ('second', 'minute', 'hour')
+    if time < 60:
+        suffix = 's' if time > 1 else ''
+        return '{t} {u}{s} ago'.format(t=time,u=unit,s=suffix)
+    return timeformat(time/60, units[units.index(unit)+1])
