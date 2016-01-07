030e87561bc9a6ac3d8c04c7252a1926243a0369
吴涵 <wuhan@xindong.com>
2016-01-07 22:07:16 +0800

    Rename lib/swarm/Containers.py, lib/swarm/Images.py, lib/swarm/Misc.py

--- lib/swarm/__init__.py
+++ lib/swarm/__init__.py
@@ -1,6 +1,6 @@
-from Misc import Version, Info
-from Containers import Containers, StartContainer, StopContainer, RestartContainer, RemoveContainer
-from Images import Images, Tag, RemoveImage
+from misc import Version, Info
+from container import Containers, StartContainer, StopContainer, RestartContainer, RemoveContainer
+from image import Images, Tag, RemoveImage
 
 __all__ = ['Version', 'Info', 'Containers', 'StartContainer',\
            'StopContainer', 'RestartContainer', 'RemoveContainer',\
